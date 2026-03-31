5e656bdc5f207be565f8a07f4927791e2e2ae08c
divyaashree8 <divyaashree8@gmail.com>
2026-03-31 17:38:34 +0530

    Added date field to expenses, better formatted output and clean total calculation.

--- PYTHON/StdE/STDET.py
+++ PYTHON/StdE/STDET.py
@@ -3,12 +3,21 @@ expenses=[]
 def addd_expense():
     amount=float(input("Enter amount:"))
     category= input("Enter category:")
-    expense={"amount": amount,"category":category}
+    date= input("Enter Date (YYYY-MM-DD):")
+    expense={"amount": amount,"category":category,"date":date}
     expenses.append(expense)
     print("Added")
 def view_expenses():
+    if not expenses:
+        print("No expenses recorded")
+        return
+    print("All Expenses")
+    print("-"*40)
+    print(f"{'date':<12}{'category':<15}{'Amount':<10}")
+    print("-"*40)
     for i in expenses:
-        print(i)
+        print(f"{i['date']:<12}{i['category']:<15}{i['amount']:<10}")
+        print("-"*40)
 def total_expenses():
     total=sum(i["amount"] for i in expenses)
     print("Total:",total)
