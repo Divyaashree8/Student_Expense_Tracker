3d3ccbdf0148593d75a372b036b249d6ebea7789
divyaashree8 <divyaashree8@gmail.com>
2026-04-16 18:14:21 +0530

    Add date field to expense tracking

--- PYTHON/StdE/flask/flaskversion.py
+++ PYTHON/StdE/flask/flaskversion.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
 app= Flask(__name__,template_folder='templates')
 expenses=[]
-def add_expense(category,amount):
-    expense={"category":category,"amount":float(amount)}
+def add_expense(category,amount,date):
+    expense={"category":category,"amount":float(amount),"date":date}
     expenses.append(expense)
 @app.route("/")
 def home():
@@ -11,7 +11,8 @@ def home():
 def add():
     category=request.form['category']
     amount= request.form['amount']
-    add_expense(category,amount)
+    date= request.form['date']
+    add_expense(category,amount,date)
     return render_template('index.html',expenses=expenses)
 if __name__=="__main__":
     app.run(debug=True)
